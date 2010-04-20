721d0420dd3f535a612a1e80391eebd87fd9d039
Jean-Christophe Hoelt <j3k0@fovea.cc>
2010-04-20 12:49:05 +0300

    Intial Wordpress Plugin

--- make_dist.sh
+++ make_dist.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 rm -f *~
-DIRECTORY="wobi-`date +%Y%m%d`"
-FILES="wobi.php wobi_functions.php Torrent.php funcsv2.php announce.php tracker.php config.php BDecode.php BEncode.php sha1lib.php"
+ZIPFILE="wobi-bittorrent-`date +%Y%m%d`.zip"
+DIRECTORY="wobi-bittorrent"
+FILES="wobi.php wobi_functions.php Torrent.php funcsv2.php announce.php tracker.php config.php BDecode.php BEncode.php sha1lib.php readme.txt"
 mkdir -p $DIRECTORY/torrents
 chmod 770 $DIRECTORY/torrents
 touch $DIRECTORY/torrents/index.php
 cp $FILES $DIRECTORY
-zip -r $DIRECTORY.zip $DIRECTORY/
+zip -r $ZIPFILE $DIRECTORY/
 rm -fr $DIRECTORY
---
+++ wobi-bittorrent.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Wobi - Bittorrent
+Plugin URI: http://www.fovea.cc/wobi
+Description: Adds a Bittorrent tracker to you Wordpress. Adds links to .torrent for Audio and videos files.
+Version: 20100420.0
+Author: Jean-Christophe Hoelt
+Author URI: http://www.fovea.cc/
+License: GPL2
+*/
+?>
+
+<?php
+/*  Copyright 2010 Jean-Christophe Hoelt (email: hoelt@fovea.cc)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
+
+
